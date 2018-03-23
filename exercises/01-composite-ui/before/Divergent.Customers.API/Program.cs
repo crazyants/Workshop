2e496e76ffa35eb7cd1d22a97ea6fb5df89a7a32
SzymonPobiega <szymon.pobiega@gmail.com>
2018-03-23 09:29:07 +0100

    Setting console title

--- exercises/01-composite-ui/before/Divergent.Customers.API/Program.cs
+++ exercises/01-composite-ui/before/Divergent.Customers.API/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
 
@@ -8,12 +9,14 @@ namespace Divergent.Customers.API
     {
         public static async Task Main(string[] args)
         {
+            Console.Title = MethodBase.GetCurrentMethod().DeclaringType.Namespace;
+
             var tcs = new TaskCompletionSource<object>();
             Console.CancelKeyPress += (sender, e) => { tcs.SetResult(null); };
 
             using (WebApp.Start<Startup>("http://localhost:20186"))
             {
-                await Console.Out.WriteLineAsync("Divergent.Customers.API web server is running.");
+                await Console.Out.WriteLineAsync("Web server is running.");
                 await Console.Out.WriteLineAsync("Press Ctrl+C to exit...");
 
                 await tcs.Task;
