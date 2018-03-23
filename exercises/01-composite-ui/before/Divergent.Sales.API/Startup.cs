22fcedcefbb14eeddd265147b460c21d9cc4d5c0
SzymonPobiega <szymon.pobiega@gmail.com>
2018-03-23 09:43:14 +0100

    Sales

--- exercises/01-composite-ui/before/Divergent.Sales.API/Startup.cs
+++ exercises/01-composite-ui/before/Divergent.Sales.API/Startup.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.Owin;
+﻿using System.Web.Http;
 using Owin;
-using Microsoft.Owin.Cors;
 
-[assembly: OwinStartup(typeof(Divergent.Sales.API.Startup))]
 
-namespace Divergent.Sales.API
+namespace Divergent.Customers.API
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseCors(CorsOptions.AllowAll);
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.EnableCors();
+
+            app.UseWebApi(config);
         }
     }
 }
