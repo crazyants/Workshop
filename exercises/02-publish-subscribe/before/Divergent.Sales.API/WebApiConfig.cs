8ee6d4e73482e1819e5c8d17cee2d3a33d6ba25d
SzymonPobiega <szymon.pobiega@gmail.com>
2018-03-27 13:03:21 +0200

    Ex 2 before

--- exercises/02-publish-subscribe/before/Divergent.Sales.API/WebApiConfig.cs
+++ exercises/02-publish-subscribe/before/Divergent.Sales.API/WebApiConfig.cs
@@ -1,17 +1,22 @@
 ﻿using System.Net.Http.Formatting;
 using System.Web.Http;
+using Castle.Windsor;
 using Newtonsoft.Json.Serialization;
 using Owin;
+using Radical.Bootstrapper.Windsor.AspNet.Infrastructure;
 
-namespace Divergent.Finance.API
+namespace Divergent.Sales.API
 {
-    public partial class Startup
+    public class WebApiConfig
     {
-        public void Configuration(IAppBuilder app)
+        public static void Configure(IAppBuilder app, IWindsorContainer container)
         {
             var config = new HttpConfiguration();
+
             config.MapHttpAttributeRoutes();
             config.EnableCors();
+
+            config.DependencyResolver = new WindsorDependencyResolver(container);
 
             config.Formatters.Clear();
             config.Formatters.Add(new JsonMediaTypeFormatter());
